c89972e25f1189a426d1728dffd6ab2ddfbd5161
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-01 14:03:11 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-black bg-blend-darken">
+        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
+        <ol className='pt-5'>
+           <li>React Router (v5) </li>
+           <li>Tailwind CSS </li>
+            <li> Configuration to use a proxy server.</li>
+            <li>React start script modified to use openssl legacy provider.</li>
+        </ol>
+      
       </header>
     </div>
   );
