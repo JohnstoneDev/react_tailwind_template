e358fc39093547fb4f701640065905123d33c2a4
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-13 02:28:03 +0300

    feat: Specify Default fonts, @tailwindcss/forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
+    fontFamily : {
+      main : ['Work Sans', 'sans-serif'],
+      headings : ['Archivo', 'sans-serif'],
+      text : ['Ranade', 'sans-serif']
+    },
+    screens: {
+      'mobile' : '375px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
